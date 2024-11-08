a217f7be443263b548f182a21ea208ad9401c61b
melzjoanne <melzjoanne@gmail.com>
2024-11-08 20:53:23 +0530

    initialized frontend & backend

---
+++ backend/RealTimeTicket/src/main/java/lk/ac/iit/RealTimeTicket/RealTimeTicketApplication.java
@@ -0,0 +1,13 @@
+package lk.ac.iit.RealTimeTicket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeTicketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeTicketApplication.class, args);
+	}
+
+}
---
+++ backend/RealTimeTicket/src/test/java/lk/ac/iit/RealTimeTicket/RealTimeTicketApplicationTests.java
@@ -0,0 +1,13 @@
+package lk.ac.iit.RealTimeTicket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeTicketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
